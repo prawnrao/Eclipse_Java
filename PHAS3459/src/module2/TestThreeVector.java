b6f1a8fdee76313d68a4cb12117827ac8d0f31bb
zcappra <zcappra@CT00017913.ad.ucl.ac.uk>
2017-10-12 10:34:46 +0100

    Scalar Product fucntion was successfully defined and calculated.

--- PHAS3459/src/module2/TestThreeVector.java
+++ PHAS3459/src/module2/TestThreeVector.java
@@ -3,13 +3,21 @@ package module2;
 public class TestThreeVector {
 
 	public static void main(String[] args) {
-		ThreeVector vect1 = new ThreeVector(1,1,1);
-		double mag1 = vect1.magnitude();
-		ThreeVector unitv1 = vect1.unitVector();
-		System.out.println("The vector is: "+vect1);
-		System.out.println("The magnitude is: "+mag1);
-		System.out.println("Unit Vector is: "+unitv1);
+		ThreeVector vect1 = new ThreeVector(1,1,1);//assigns a value to vect1
+		ThreeVector vect2 = new ThreeVector(2,2,2);//assigns a value to vect2
 		
+		double mag1 = vect1.magnitude();//calculates the magnitude of vect1
+		ThreeVector unitv1 = vect1.unitVector();//calculates the unit vector of vect1
+		
+		System.out.println("Vector1 is: "+vect1);
+		System.out.println("The magnitude of Vector1 is: "+mag1);
+		System.out.println("Unit Vector1 is: "+unitv1);
+		
+		double scalar_product1 = vect1.scalarProduct(1,1,1,2,2,2);//calculates the scalar product between two vectors using method 1
+		double scalar_product = vect1.scalarProduct(vect1, vect2);//calculates the scalar product between vect1 and vect2 using method 2
+		
+		System.out.println("Scalar Product is: "+scalar_product);
+		System.out.println("Scalar Product between vect1 and vect2 is: "+scalar_product1);
 	}
 
 }
--- PHAS3459/src/module2/ThreeVector.java
+++ PHAS3459/src/module2/ThreeVector.java
@@ -11,7 +11,6 @@ public class ThreeVector {
 		this.z = z;
 	}
 
-	
 	//This calculates the magnitude of the Three Vector
 	double magnitude () {
 		double mag = Math.sqrt(x*x + y*y + z*z);
@@ -19,6 +18,7 @@ public class ThreeVector {
 		return mag;
 	}
 	
+	//This calculates the unit vector of the Three Vector
 	ThreeVector unitVector() {
 		double unitx = x/mag;
 		double unity = y/mag;
@@ -26,11 +26,19 @@ public class ThreeVector {
 		return new ThreeVector(unitx,unity,unitz);	
 	}
 	
-	public String toString() {
-		return "("+x+","+y+","+z+")";
+	//This piece of code calculates the scalar product between 2 Three Vectors that are manually inputed 
+	double scalarProduct(double x1,double y1,double z1,double x2,double y2,double z2) {
+		double SProd = x1*x2+y1*y2+z1*z2;
+		return SProd;
+	}
+	//This piece of code overloads the scalarProduct function, and allows it to take 2 ThreeVector variables to calculate the scalar product
+	double scalarProduct(ThreeVector v1,ThreeVector v2) {
+		double SProd = v1.x*v2.x+v1.y*v2.y+v1.z*v2.z;
+		return SProd;
 	}
 	
-	double scalarProduct() {
-		double SProd = 
+	//This is the toString code, which allows to print all the ThreeVector values easily
+	public String toString() {
+		return "("+x+","+y+","+z+")";
 	}
 }
