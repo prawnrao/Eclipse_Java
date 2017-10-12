d43fc73082542c5006bd28c41a072fda4a91137e
Pranav Rao <zcappra@ucl.ac.uk>
2017-10-12 11:22:35 +0100

    Comments were added and static and non static values were calculated correctly

--- PHAS3459/src/module2/TestThreeVector.java
+++ PHAS3459/src/module2/TestThreeVector.java
@@ -3,21 +3,39 @@ package module2;
 public class TestThreeVector {
 
 	public static void main(String[] args) {
-		ThreeVector vect1 = new ThreeVector(1,1,1);//assigns a value to vect1
-		ThreeVector vect2 = new ThreeVector(2,2,2);//assigns a value to vect2
+		ThreeVector v1 = new ThreeVector(3,5,2);//assigns a value to v1
+		ThreeVector v2 = new ThreeVector(2,4,1);//assigns a value to v2
+		ThreeVector v3 = new ThreeVector(0,0,0);//assigns a value to v3
+				
+		ThreeVector unitv1 = ThreeVector.unitVector(v1);//calculates the unit vector of vect1
+		ThreeVector unitv2 = ThreeVector.unitVector(v2);//calculates the unit vector of vect2
+		ThreeVector unitv3 = ThreeVector.unitVector(v3);//calculates the unit vector of vect3
 		
-		double mag1 = vect1.magnitude();//calculates the magnitude of vect1
-		ThreeVector unitv1 = vect1.unitVector();//calculates the unit vector of vect1
-		
-		System.out.println("Vector1 is: "+vect1);
-		System.out.println("The magnitude of Vector1 is: "+mag1);
+		//This code block prints out the values of v1,v2,v3 and their unit vectors
+		System.out.println("Vector1 is: "+v1);
 		System.out.println("Unit Vector1 is: "+unitv1);
+		System.out.println("\nVector2 is: "+v2);
+		System.out.println("Unit Vector2 is: "+unitv2);
+		System.out.println("\nVector3 is: "+v3);
+		System.out.println("Unit Vector3 is: "+unitv3);
 		
-		double scalar_product1 = vect1.scalarProduct(1,1,1,2,2,2);//calculates the scalar product between two vectors using method 1
-		double scalar_product = vect1.scalarProduct(vect1, vect2);//calculates the scalar product between vect1 and vect2 using method 2
+		double scalar_product_12 = v1.scalarProduct(v2);//calculates the non-static scalar product between v1 and v2
+		double scalar_product_13 = v1.scalarProduct(v3);//calculates the non-static scalar product between v1 and v3
 		
-		System.out.println("Scalar Product is: "+scalar_product);
-		System.out.println("Scalar Product between vect1 and vect2 is: "+scalar_product1);
+		System.out.println("\nScalar Product using non-static method between v1 and v2 is: "+scalar_product_12);
+		System.out.println("Scalar Product using non-static method between v1 and v3 is: "+scalar_product_13);
+		
+		double static_scalar_product_12 = ThreeVector.scalarProduct(v1, v2);//calculates the static scalar product between v1 and v2
+		double static_scalar_product_13 = ThreeVector.scalarProduct(v1, v3);//calculates the static scalar product between v1 and v3
+		
+		System.out.println("\nScalar Product using static method between v1 and v2 is: "+static_scalar_product_12);
+		System.out.println("Scalar Product using static method between v1 and v3 is: "+static_scalar_product_13);
+
+		ThreeVector vector_product_12 = ThreeVector.vectorProduct(v1, v2) ;
+		ThreeVector vector_product_13 = ThreeVector.vectorProduct(v1, v3) ;
+		
+		System.out.println("\nVector Product between v1 and v2 is: "+ vector_product_12);
+		System.out.println("Vector Product between v1 and v3 is: "+ vector_product_13);
 	}
 
 }
--- PHAS3459/src/module2/ThreeVector.java
+++ PHAS3459/src/module2/ThreeVector.java
@@ -12,29 +12,36 @@ public class ThreeVector {
 	}
 
 	//This calculates the magnitude of the Three Vector
-	double magnitude () {
-		double mag = Math.sqrt(x*x + y*y + z*z);
-		this.mag = mag;
+	static double magnitude (ThreeVector v) {
+		double mag = Math.sqrt(v.x*v.x + v.y*v.y + v.z*v.z);
+		
 		return mag;
 	}
 	
 	//This calculates the unit vector of the Three Vector
-	ThreeVector unitVector() {
-		double unitx = x/mag;
-		double unity = y/mag;
-		double unitz = z/mag;
+	static ThreeVector unitVector(ThreeVector v) {
+		double unitx = v.x/magnitude(v);
+		double unity = v.y/magnitude(v);
+		double unitz = v.z/magnitude(v);
 		return new ThreeVector(unitx,unity,unitz);	
 	}
 	
 	//This piece of code calculates the scalar product between 2 Three Vectors that are manually inputed 
-	double scalarProduct(double x1,double y1,double z1,double x2,double y2,double z2) {
-		double SProd = x1*x2+y1*y2+z1*z2;
+	double scalarProduct(ThreeVector v) {
+		double SProd = v.x*this.x+v.y*this.y+v.z*this.z;
 		return SProd;
 	}
 	//This piece of code overloads the scalarProduct function, and allows it to take 2 ThreeVector variables to calculate the scalar product
-	double scalarProduct(ThreeVector v1,ThreeVector v2) {
+	static double scalarProduct(ThreeVector v1,ThreeVector v2) {
 		double SProd = v1.x*v2.x+v1.y*v2.y+v1.z*v2.z;
 		return SProd;
+	}
+	
+	static ThreeVector vectorProduct(ThreeVector v1, ThreeVector v2) {
+		double VProdx = (v1.y*v2.z)-(v1.z*v2.y);
+		double VPrody = (v1.z*v2.x)-(v1.x*v2.z);
+		double VProdz = (v1.x*v2.y)-(v1.y*v2.x);
+		return new ThreeVector(VProdx, VPrody,VProdz);
 	}
 	
 	//This is the toString code, which allows to print all the ThreeVector values easily
