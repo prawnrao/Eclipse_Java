4c4ae93d4f4b1cef35215a9b28de92898d621138
Pranav Rao <zcappra@ucl.ac.uk>
2017-10-19 10:37:37 +0100

    Module 3 Thursday mid-session class Alphabet commit after completing randomchar

--- PHAS3459/src/module3/Alphabet.java
+++ PHAS3459/src/module3/Alphabet.java
@@ -1,5 +1,22 @@
 package module3;
 
-public class Alphabet {
+import java.util.Random;
 
+public class Alphabet {
+	
+	//method to get a random character
+	static char randomChar() {
+		Random rand = new Random();
+		char randChar = (char) rand.nextInt(128);//casts an integer between 0-127 to a character
+		return randChar;
+	}
+	
+	public static void main(String[] args) {
+		
+		char char1 = 'a' ;//defines new character
+		char1 = randomChar();//assigns char1 a random character
+		System.out.println("Random character: "+char1);
+		
+		
+	}
 }
