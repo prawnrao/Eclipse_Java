9295313c288cec75e5087c719c92e580786456db
Pranav Rao <zcappra@ucl.ac.uk>
2017-10-26 11:43:18 +0100

    Module 4: working on the pint writer

--- PHAS3459/src/module4/NumericalReader.java
+++ PHAS3459/src/module4/NumericalReader.java
@@ -14,6 +14,26 @@ public class NumericalReader {
 		String s = br.readLine();
 		return s;
 	}
+	
+	public BufferedReader brFromURL(String urlName) throws IOException {
+		URL url = new URL(urlName);
+		InputStream is = url.openStream();
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader url_br = new BufferedReader(isr);
+		return url_br;
+	}
+	
+	void analysisStart(String datafile) throws IOException {
+		FileWriter fw = new FileWriter(datafile);
+		BufferedWriter bw = new BufferedWriter(fw);
+		double minValue = 0;
+		double maxValue = 0;
+		double nValues = 0;
+		double sumOfValues = 0;
+		PrintWriter pw = new PrintWriter(bw);
+		pw.format("The minimum value is: %d"+"The maximum value is: %d "+"The number of values is: %d"+"The sum of the values is: %d",minValue,maxValue,nValues,sumOfValues);
+		pw.close();
+	}
 
 	public static void main(String[] args) {
 		try {
