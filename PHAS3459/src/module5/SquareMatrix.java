8272bb0252722adfbcaca31df4a4e872c8b2bb4a
Pranav Rao <zcappra@ucl.ac.uk>
2017-11-01 13:45:18 +0000

    Module 5: End of Wednesday Session, constructor, and show() are complete

--- PHAS3459/src/module5/SquareMatrix.java
+++ PHAS3459/src/module5/SquareMatrix.java
@@ -1,6 +1,7 @@
 package module5;
 
 public class SquareMatrix {
+	//Member variables
 	private int rows;
 	private int columns;
 	private double [][] elements;
@@ -10,23 +11,37 @@ public class SquareMatrix {
 	//and throws an exception if the number of columns is inconsistent
 	public SquareMatrix(double[][]elements) throws Exception{
 		this.elements = elements;
-		this.rows = elements.length;
-
-		this.columns = elements[0].length;
-		int i = 0;
+		int rows = elements.length;//number of rows
+		int columns = elements[0].length;//number of columns in row 0
+		
+		int i = 0; //initialises the counter
 		//while loop, that checks the number of columns in each row
-		while(i < this.rows ) {
-			int columns = elements[i].length;//checks the number of columns in row i
-			if (this.columns != columns) {
-				throw new Exception("The number of columns in each row is not equal. Not a physical matrix.");
+		while(i < rows ) {
+			this.columns = elements[i].length;//checks the number of columns in row i
+			if (this.columns != columns) {//checks the number of columns in row 0 against the number of columns in row i
+				throw new Exception("The number of columns in each row is not equal. Not a valid matrix.");
 			}
-			i++;
+			i++;//increments counter
 		}
-		if(rows != columns) {
+		if(rows != columns) {//checks the number of rows against the number of columns
 			throw new Exception("The number of rows is not equal to the number of columns. Not a square matrix");
 		}
 
 	}
-
+	
+	//Prints matrix to screen
+	//TODO 
+	//ASK IF YOU CAN DO THIS INSTEAD OF TOSTRING
+	public void show() {
+		System.out.println();
+		for (int i = 0; i < elements.length; i++) {
+			for (int j = 0; j < elements.length; j++) 
+				System.out.printf("%10.1f", elements[i][j]);
+			System.out.println();
+		}
+		System.out.println();
+		System.out.println();
+		
+	}
 
 }
--- PHAS3459/src/module5/TestSquareMatrix.java
+++ PHAS3459/src/module5/TestSquareMatrix.java
@@ -2,4 +2,50 @@ package module5;
 
 public class TestSquareMatrix {
 
+	public static void main (String[] args) {
+		double[][] a  = {{1,2,0},{0,2,0},{-2,0,1}};
+		double[][] b  = {{2,1,0},{0,1,0},{-1,0,1}};
+		double[][] c  = {{4,3},{3,2}};
+		double[][] d  = {{-2,3},{3,-4}};
+
+		SquareMatrix A = null;
+		try {
+			A = new SquareMatrix(a);
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+		System.out.println("Matrix A:");
+		A.show();
+
+		SquareMatrix B = null;
+		try {
+			B = new SquareMatrix(b);
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+		System.out.println("Matrix B:");
+		B.show();
+
+		SquareMatrix C = null;
+		try {
+			C = new SquareMatrix(c);
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+		System.out.println("Matrix C:");
+		C.show();
+
+		SquareMatrix D = null;
+		try {
+			D = new SquareMatrix(d);
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+		System.out.println("Matrix D:");
+		D.show();
+	}
 }
