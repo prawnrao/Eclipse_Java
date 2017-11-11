56899ef70a3b834e80427fe1af3a18c7c6eda839
Pranav Rao <zcappra@ucl.ac.uk>
2017-11-11 13:31:58 +0000

    Module 5: goodness of fit is complete

--- PHAS3459/src/module5/DataAnalysis.java
+++ PHAS3459/src/module5/DataAnalysis.java
@@ -17,69 +17,49 @@ public class DataAnalysis {
 	 * @return
 	 * @throws IOException
 	 */
-	public static ArrayList<Double> dataFromURL(String urlName) throws IOException {
+	public static ArrayList<DataPoint> dataFromURL(String urlName) throws IOException {
 		URL url = new URL(urlName);
 		InputStream is = url.openStream();
 		InputStreamReader isr = new InputStreamReader(is);
 		BufferedReader br = new BufferedReader(isr);
-		String line = "";
-		ArrayList<Double> url_data = new ArrayList<>();
-		while ((line = br.readLine()) != null) {//Loops until the buffered reader is empty
-			Scanner s = new Scanner(line);
-			while (s.hasNext()) {//Loops until the scanner has another token
-				double x = Double.parseDouble(s.next());//resolves the string into a double
-				url_data.add(x);
+		ArrayList<Double> url_data = new ArrayList<Double>();
+		ArrayList<DataPoint> datapoints = new ArrayList<DataPoint>();
+		Scanner s = new Scanner(br);
+
+		while (s.hasNext()) {//Loops while the scanner has another token
+			double val = Double.parseDouble(s.next());//resolves the string into a double
+			url_data.add(val);
+			if (url_data.size() >= 3){
+				DataPoint point = new DataPoint(url_data.get(0),url_data.get(1),url_data.get(2));
+				datapoints.add(point);
+				url_data = new ArrayList<Double>();
 			}
-			s.close();
 		}
-		return url_data;
+		s.close();
+		return datapoints;
 	}
 
-	public static double goodnessfFit(Theory n, ArrayList<DataPoint> datapoint) {
-		
-		return 0;
-		
+	public static double goodnessfFit(Theory n, ArrayList<DataPoint> datapoints) {
+		double xpoint,ypoint, eypoint, residual;
+		double chi_sq = 0;
+
+		for(int i=0; i <= datapoints.size();i++){
+			residual = 0;
+			DataPoint point = datapoints.get(i);
+			xpoint = point.getX();
+			ypoint = point.getY();
+			eypoint = point.getEY();
+			double y_theory = n.y(xpoint);		
+			residual = (Math.pow((ypoint - y_theory),2))/(Math.pow(eypoint, 2));
+			chi_sq += residual;
+		}
+		return chi_sq;
+
 	}
 
 	public static void main(String[] args) {
-		ArrayList<Double> data = new ArrayList<>();//Defines a new array list with double values
-		try {
-			data = dataFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module5/module5-xy.txt");//imports data from the url to the array list
-			System.out.println("All data from the URL:\n\t"+data);//outputs the data to screen
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		ArrayList<DataPoint> data = new ArrayList<>();//Defines a new array list with double values
 
-
-		//initialises variables used in the loops
-		ArrayList<Double> xdata = new ArrayList<>();
-		ArrayList<Double> ydata = new ArrayList<>();
-		ArrayList<Double> eydata = new ArrayList<>();
-		int n= 0;
-		
-		while(n < data.size()) {//loops for the entire size of the array
-			int i = 0;
-			while(i < 3) {//loops every 3 integers
-				if(i == 0) {//catches the 0th, 3rd, 6th... index of the array list
-					double x = data.get(n);
-					xdata.add(x);//appends the x data array list with the value of index n
-				}
-				else if(i == 1) {//catches the 1st, 4th, 7th... index of the data array list
-					double y = data.get(n);
-					ydata.add(y);//appends the y data array list with the value of index n
-				}
-				else if (i==2) {//catches the 2nd, 5th, 8th... index of the data array list
-					double ey = data.get(n); 
-					eydata.add(ey);//appends the error y data array list with the value of index n
-				}
-				i++;//increments i
-				n++;//increments n
-			}
-		}
-
-		System.out.println("\nX data from URL:\n\t"+xdata);
-		System.out.println("\nY data from URL:\n\t"+ydata);
-		System.out.println("\neY data from URL:\n\t"+eydata);
 	}
 
 }
